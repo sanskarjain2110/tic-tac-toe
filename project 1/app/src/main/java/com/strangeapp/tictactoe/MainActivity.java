a3f14e8c0480d1ed43e21182cb12e247c7621f42
Sanskar <sanskarjain505@gmail.com>
2022-01-17 01:53:20 +0530

    human vs human complet

--- project 1/app/src/main/java/com/strangeapp/tictactoe/MainActivity.java
+++ project 1/app/src/main/java/com/strangeapp/tictactoe/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     public static int[] cell_status = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void user_click(View view) {
         ImageView img = (ImageView) view;
+        TextView status = findViewById(R.id.status);
         int cell_pos = Integer.parseInt(img.getTag().toString());
 
         // check weather cell is empty or not
@@ -40,15 +44,17 @@ public class MainActivity extends AppCompatActivity {
                     img.setImageResource(R.drawable.x);
                     cell_status[cell_pos] = 1;
                     turn = -1;
+                    status.setText("O's Turn - Tap to play");
                     moves_left--;
                 } else if (turn == -1) {
                     img.setImageResource(R.drawable.o);
                     cell_status[cell_pos] = -1;
                     turn = 1;
+                    status.setText("X's Turn - Tap to play");
                     moves_left--;
                 }
 
-                win_condition();
+                win_condition(status);
 
             } else {
                 Toast.makeText(MainActivity.this, "reset the game", Toast.LENGTH_SHORT).show();
@@ -58,21 +64,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void win_condition() {
+    public void win_condition(TextView status) {
         if (moves_left > 0) {
             for (int i = 0; i < 8; i++) {
                 if (cell_status[win_positions[i][0]] != 0 &&
                         cell_status[win_positions[i][0]] == cell_status[win_positions[i][1]] &&
-                        cell_status[win_positions[i][0]] == cell_status[win_positions[i][0]]) {
-                    Toast.makeText(MainActivity.this, "win", Toast.LENGTH_SHORT).show();
+                        cell_status[win_positions[i][0]] == cell_status[win_positions[i][2]]) {
+
+                    if (cell_status[win_positions[i][0]] == -1) {
+                        status.setText("O wins - Tap to reset game");
+                    } else {
+                        status.setText("X wins - Tap to reset game");
+                    }
                     turn = 0;
                     break;
                 }
             }
         } else {
-            Toast.makeText(MainActivity.this, "it's draw", Toast.LENGTH_SHORT).show();
+            status.setText("Draw - Tap to reset game");
 
         }
-
     }
+
+   public void reset(View view) {
+       if (turn == 0) {
+           Arrays.fill(cell_status, 0);
+
+           ((ImageView) findViewById(R.id.tap0)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap1)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap2)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap3)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap4)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap5)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap6)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap7)).setImageResource(0);
+           ((ImageView) findViewById(R.id.tap8)).setImageResource(0);
+
+           turn = 1;
+           moves_left = 9;
+
+           TextView status = findViewById(R.id.status);
+           status.setText("X's Turn - Tap to play");
+
+       }
+   }
 }
